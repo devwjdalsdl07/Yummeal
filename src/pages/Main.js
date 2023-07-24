da4ca435d507a28d16b480485a67f76fa5a4e3dd
jd0097 <dh.react123@gmail.com>
2023-07-24 21:58:37 +0900

    main emotion 적용

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { MainDiv } from "../style/MainDiv";
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+  };
 
-export default Main
+  return (
+    <MainDiv>
+      <div className="wrap">
+        <div className="container">
+          <h2>ㅇㅇ님의 추천 상품</h2>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
+          <h2>요즘, 많이 찾는 상품</h2>
+          <h2>추천 상품</h2>
+        </div>
+      </div>
+    </MainDiv>
+  );
+};
+
+export default Main;
---
+++ src/style/MainDiv.js
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+export const MainDiv = styled.div`
+  .wrap {
+    position: relative;
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
+    .container {
+      margin: 0 auto;
+      margin-top: 50px;
+      width: 100%;
+      max-width: 1400px;
+      height: 100%;
+      min-height: 700px;
+      padding-bottom: 200px;
+    }
+  }
+`;
