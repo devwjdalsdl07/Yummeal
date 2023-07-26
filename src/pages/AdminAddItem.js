83ba4bdee05d6d7d2bf08ac9b72029d13b86410f
dev2hearts <js.park119de@gmail.com>
2023-07-26 19:34:00 +0900

    feat: Mypage 작업 중 & additem 기능 완료

--- src/pages/AdminAddItem.js
+++ src/pages/AdminAddItem.js
@@ -1,12 +1,35 @@
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import ImgUpload from "../components/ImgUpload";
 import { AdminWrapper } from "../style/AdminCss";
 
 const AdminAddItem = () => {
+  // 카테고리 더미데이터
+  const exCateValue = [
+    { value: 1, cate: "하하" },
+    { value: 2, cate: "호호" },
+    { value: 3, cate: "힝힝" },
+  ];
   const quillRef = useRef();
   const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState();
+  const [commaPrice, setCommaPrice] = useState();
+  const [cate, setCate] = useState("");
+  const handleTitleChange = e => {
+    setTitle(e.target.value);
+  };
+  const handlePriceChange = e => {
+    const value = e.target.value;
+    const removedCommaValue = Number(value.replaceAll(/[^0-9]/g, ""));
+    setCommaPrice(removedCommaValue.toLocaleString());
+    setPrice(removedCommaValue);
+  };
+  const handleCateChange = e => {
+    setCate(e.target.value);
+  };
+  
   const modules = useMemo(() => {
     return {
       toolbar: {
@@ -26,19 +49,38 @@ const AdminAddItem = () => {
   }, []);
   return (
     <AdminWrapper>
-      <div>
+      <div className="titleArea">
         <div className="uploadContainer">
           <ImgUpload />
           <ImgUpload />
           <ImgUpload />
           <ImgUpload />
         </div>
-        <div>title,price,cate</div>
+        <div>
+          <input
+            type="text"
+            value={title}
+            onChange={e => handleTitleChange(e)}
+          ></input>
+          <input
+            type="text"
+            value={commaPrice}
+            onChange={e => handlePriceChange(e)}
+          ></input><p>원</p>
+          <select onChange={handleCateChange}>
+            <option value="">카테고리</option>
+            {exCateValue.map((item, idx) => (
+              <option key={idx} value={item.value}>
+                {item.cate}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-      <div style={{ margin: "50px" }}>
+      <div className="editorWrapper">
         <button onClick={() => console.log(content)}>Value</button>
         <ReactQuill
-          style={{ width: "600px", height: "600px" }}
+          style={{ width: "800px", height: "2000px" }}
           placeholder="Quill Content"
           theme="snow"
           ref={quillRef}
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import "../style/MypageCss";
+import { MypageContainer } from "../style/MypageCss";
 
 const Mypage = () => {
   return (
-    <div>Mypage</div>
-  )
-}
+    <MypageContainer>
+      <div className="side-nav">
+        <h1>마이 페이지</h1>
+        <ul className="menu-list">
+          <li>
+            <p>나의 주문 관리</p>
+            <ul className="two-depth">
+              <li>
+                <p>주문 배송</p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>나의 정보 관리</p>
+            <ul className="two-depth">
+              <li>
+                <p>정보 수정</p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div></div>
+    </MypageContainer>
+  );
+};
 
-export default Mypage
+export default Mypage;
