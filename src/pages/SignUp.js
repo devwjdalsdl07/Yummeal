c77c6908652b75247ebcb915c6f7d0556837473b
devwjdalsdl07 <dev.wjdalsdl07@gmail.com>
2023-07-27 18:10:50 +0900

    feat: 회원가입 antD datePicker 적용 및 카카오 주소 API 적용

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   JoinArea,
   JoinBtn,
   JoinContainer,
   JoinFormGroup,
   JoinId,
+  JoinPost,
   JoinPw,
   JoinPwConfirm,
   JoinText,
@@ -14,12 +15,76 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router";
+import { DatePicker, Space } from "antd";
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [postcode, setPostcode] = useState("");
+  const [address, setAddress] = useState("");
+  const [detailAddress, setDetailAddress] = useState("");
+  const [extraAddress, setExtraAddress] = useState("");
+
+  useEffect(() => {
+    // Daum 우편번호 스크립트를 동적으로 로드
+    const script = document.createElement("script");
+    script.src =
+      "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+    document.body.appendChild(script);
+
+    // 스크립트가 로드되면 실행할 콜백 함수
+    script.onload = () => {
+      // Daum 우편번호 스크립트가 로드된 후에는 여기에서 코드를 실행할 수 있습니다.
+      // 여기서 다음 스크립트를 사용하여 우편번호 찾기 기능을 구현할 수 있습니다.
+    };
+  }, []);
   const handleSignUp = () => {
     navigate("/Login");
   };
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
+  const handleExecDaumPostcode = () => {
+    new window.daum.Postcode({
+      oncomplete: function (data) {
+        let addr = ""; // 주소 변수
+        let extraAddr = ""; // 참고항목 변수
+
+        // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+        if (data.userSelectedType === "R") {
+          // 사용자가 도로명 주소를 선택했을 경우
+          addr = data.roadAddress;
+        } else {
+          // 사용자가 지번 주소를 선택했을 경우(J)
+          addr = data.jibunAddress;
+        }
+
+        // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+        if (data.userSelectedType === "R") {
+          if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+            extraAddr += data.bname;
+          }
+          if (data.buildingName !== "" && data.apartment === "Y") {
+            extraAddr +=
+              extraAddr !== "" ? ", " + data.buildingName : data.buildingName;
+          }
+          if (extraAddr !== "") {
+            extraAddr = " (" + extraAddr + ")";
+          }
+          setExtraAddress(extraAddr);
+        } else {
+          setExtraAddress("");
+        }
+
+        // 우편번호와 주소 정보를 상태에 저장한다.
+        setPostcode(data.zonecode);
+        setAddress(addr);
+        // 커서를 상세주소 필드로 이동한다.
+        document.getElementById("sample6_detailAddress").focus();
+      },
+    }).open();
+  };
+
   return (
     <JoinContainer>
       <JoinArea>
@@ -114,7 +179,64 @@ const SignUp = () => {
               />
             </div>
             {/* 생년월일 드랍박스 들어갈 자리 */}
+            <div>
+              <span>
+                <i>
+                  <FontAwesomeIcon icon={faCircle} />
+                </i>
+                아이 생년월일
+              </span>
+              <Space direction="vertical">
+                <DatePicker
+                  onChange={onChange}
+                  placeholder="YYYY-MM-DD"
+                  style={{
+                    height: "30px",
+                    marginTop: "10px",
+                  }}
+                />
+              </Space>
+              {/* <input
+                type="text"
+                placeholder="이메일을 입력하세요"
+                maxLength={100}
+              /> */}
+            </div>
           </JoinFormGroup>
+          <div>
+            <input
+              type="text"
+              id="sample6_postcode"
+              value={postcode}
+              placeholder="우편번호"
+            />
+            <input
+              type="button"
+              onClick={handleExecDaumPostcode}
+              value="우편번호 찾기"
+            />
+            <br />
+            <input
+              type="text"
+              id="sample6_address"
+              value={address}
+              placeholder="주소"
+            />
+            <br />
+            <input
+              type="text"
+              id="sample6_detailAddress"
+              value={detailAddress}
+              onChange={e => setDetailAddress(e.target.value)}
+              placeholder="상세주소"
+            />
+            <input
+              type="text"
+              id="sample6_extraAddress"
+              value={extraAddress}
+              placeholder="참고항목"
+            />
+          </div>
           <JoinBtn onClick={handleSignUp}>회원가입</JoinBtn>
         </JoinWrap>
       </JoinArea>
