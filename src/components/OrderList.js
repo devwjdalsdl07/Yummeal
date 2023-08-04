f1644e11598317d4085f0db080e10702287cb15d
dev2hearts <js.park119de@gmail.com>
2023-08-04 13:03:31 +0900

    feat: mypage 반응형 완료

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,29 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { OrderListContainer } from "../style/OrderListCss";
 import OrderNumber from "./OrderNumber";
 
 const OrderList = () => {
-  // 선택된 날짜 기간 동안의 주문 목록 수량
-  const [orderCount, setOrderCount] = useState(0);
   const button = [
     { value: 1, date: "1개월" },
-    { value: 2, date: "6개월" },
-    { value: 3, date: "1년" },
-    { value: 4, date: "전체" },
+    { value: 3, date: "6개월" },
+    { value: 6, date: "1년" },
+    { value: 0, date: "전체" },
   ];
   const [active, setActive] = useState(1);
-  // orderNumver 컴포넌트 더미데이터
-  const [orderList, setOrderList] = useState([1, 2, 3, 4]);
-
+  // orderNumber 컴포넌트 더미데이터
+  const [orderList, setOrderList] = useState([
+    {
+      OrderNumber: "32498-48749",
+      date: "2023-06-15",
+      itemList: [1, 2, 3],
+      price: "100,000",
+      delivery: "배송중",
+      name: "밀푀유나베",
+    },
+    {
+      OrderNumber: "23897-32894",
+      date: "2023-04-15",
+      itemList: [1, 2],
+      price: "200,000",
+      delivery: "배송중",
+      name: "소고기 무국",
+    },
+    {
+      OrderNumber: "39857-58193",
+      date: "2023-01-15",
+      itemList: [1],
+      price: "300,000",
+      delivery: "배송중",
+      name: "로제 파스타 안심 스테이크",
+    },
+  ]);
+  const [filteredOrderList, setFilteredOrderList] = useState(orderList);
   const handleDateClick = value => {
     setActive(value);
+    filterDate(value);
   };
+  const filterDate = active => {
+    const now = new Date();
+    const filteredOrders = orderList.filter(order => {
+      const orderDate = new Date(order.date);
+      switch (active) {
+        case 1:
+          return (now - orderDate) / (1000 * 60 * 60 * 24 * 30) <= 1; // 1개월
+        case 3:
+          return (now - orderDate) / (1000 * 60 * 60 * 24 * 30) <= 6; // 6개월
+        case 6:
+          return (now - orderDate) / (1000 * 60 * 60 * 24 * 365) <= 1; // 1년
+        case 0:
+          return true; // 전체
+        default:
+          return (now - orderDate) / (1000 * 60 * 60 * 24 * 30) <= 1;
+      }
+    });
+    setFilteredOrderList(filteredOrders);
+  };
+  useEffect(() => {
+    filterDate(active);
+  }, []);
   return (
     <OrderListContainer>
       <div className="top">
         <h3>주문 배송</h3>
         <span>
-          <strong>{orderCount}</strong>
+          <strong>{filteredOrderList.length}</strong>
         </span>
         <span>개</span>
       </div>
@@ -39,9 +85,15 @@ const OrderList = () => {
           </span>
         ))}
       </div>
-      {orderList.map((item, idx) => {
-        return <OrderNumber key={idx} />;
-      })}
+      {filteredOrderList.length === 0 ? (
+        <div className="text-wrap">
+          <span className="">기간내 주문한 상품이 없습니다.</span>
+        </div>
+      ) : (
+        filteredOrderList.map((item, idx) => (
+          <OrderNumber key={idx} item={item} />
+        ))
+      )}
     </OrderListContainer>
   );
 };
--- src/components/OrderNumber.js
+++ src/components/OrderNumber.js
@@ -1,39 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import { OrderNumberContainer } from "../style/OrderNumberCss";
 
-const OrderNumber = () => {
-  const [delivery, setDelivery] = useState("배송 중");
-  const itemList = [1, 2];
+const OrderNumber = ({ item }) => {
   return (
     <OrderNumberContainer>
       <div className="orderInfo">
-        <div className="orderDate">
-          <p>주문날짜</p>
-          <span>{"2020-05-03"}</span>
-        </div>
-        <div className="orderNum">
-          <p>주문번호</p>
-          <span>{129804129842189}</span>
+        <div className="responsive-1">
+          <div className="orderDate">
+            <p>주문날짜</p>
+            <span>{item.date}</span>
+          </div>
+          <div className="orderNum">
+            <p>주문번호</p>
+            <span>{item.OrderNumber}</span>
+          </div>
         </div>
       </div>
       <div className="itemInfo">
         <div className="imgWrap">
-          <img src="http://fpoimg.com/80x80"></img>
+          <img src={item.thumNail} alt="썸네일"></img>
         </div>
         <div className="titleWrap">
           <p>제품 명</p>
-          <span>밀푀유나베(1~2인)</span>
-          {itemList.length > 1 ? <span>외{itemList.length - 1}개</span> : null}
+          <span>{item.name}</span>
         </div>
       </div>
-      <div className="orderPrice">
-        <span>
-          <strong>123,125,123</strong>
-        </span>
-        <span>원</span>
-      </div>
-      <div className="delivery">
-        <span>{delivery}</span>
+      <div className="responsive-2">
+        <div className="orderPrice">
+          <span>
+            <strong>{item.price}</strong>
+          </span>
+          <span>원</span>
+        </div>
+        <div className="delivery">
+          <span>{item.delivery}</span>
+        </div>
       </div>
     </OrderNumberContainer>
   );
